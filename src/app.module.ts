198459f88c7ab38493e79d2b617685e3a5aca7c5
seraf22 <109837540+seraf22@users.noreply.github.com>
2023-01-19 23:30:48 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './core/database/database.module';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    DatabaseModule,
+    AuthModule,
+  ],
+  controllers: [AppController, UsersController, AuthController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+})
+export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import User from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { USER_REPOSITORY } from 'src/core/constants';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @Inject(USER_REPOSITORY) private readonly usersRepository: typeof User,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const hash = await bcrypt.hashSync(createUserDto.password, 10);
+
+      return await this.usersRepository.create({
+        ...createUserDto,
+        password: hash,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.findAll<User>();
+  }
+
+  async findOne(id: number) {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+
+  async findOneByEmail(email: string) {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
